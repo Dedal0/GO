4864019aec00dd266768c31dbff676e783c12a9e
Camilo Galdos AkA Dedalo <seguridadblanca@gmail.com>
2015-06-09 20:52:38 -0500

    Create sum.go

---
+++ sum.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var num,num2,sum int
+	fmt.Println("Ingrese un numero: ")
+	fmt.Scanf("%d",&num)
+	fmt.Println("Ingrese un numero: ")
+	fmt.Scanf("%d",&num2)
+	sum = num + num2
+	fmt.Println("La suma da",sum, "hola")
+}
